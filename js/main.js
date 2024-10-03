92b7506cc7a370046e991aa5a3f2ebc2ddd8fa3d
Aamilah Dawood <78200595+Aamilah@users.noreply.github.com>
2024-10-03 16:28:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -382,4 +382,12 @@ function getCurrentDateFormatted() {
     }
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const progressBars = document.querySelectorAll('.progress');
+
+  progressBars.forEach(bar => {
+    let percentage = bar.getAttribute('data-percentage');
+    bar.style.width = percentage + '%';
+  });
+});
 
